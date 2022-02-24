f1e9a4189ce5dfcf53b735ce95bab6a582e5aa11
신근재 <shin75492@naver.com>
2022-02-24 15:24:33 +0900

    ToDo List

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,10 +1,41 @@
 import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) =>  {
+    event.preventDefault();
+    if(toDo ===""){
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");//초기화 
+  }
+
+
   return (
     <div>
-      <h1>즐거운 인생</h1>
-      <Button text={"Continue"}/>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          text="text" 
+          placeholder="해야할 일을 작성하세요!">
+        </input>
+
+        <button>할 일 추가</button>
+      </form>
+      <hr />
+
+      <ul>
+        {toDos.map((String, index) => (
+         <li key={index}>{String}</li>
+        ))}
+      </ul>
     </div>
   );
 }
