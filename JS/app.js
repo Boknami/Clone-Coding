75ee1f63a23b4839dc75e5d93e96f5cfe22925e4
신근재 <shin75492@naver.com>
2022-03-17 00:48:32 +0900

    아아아큰일났다
    ㄴㅇㄻ

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,14 @@
-const title = document.getElementById("title");
+const h1 = document.querySelector(".title h1")
 
-title.innerText = "G";
+function Run(){
+    const h1sColor = h1.style.color;
+
+    if (h1sColor === "blue"){
+        h1sColor = "orange";
+    }
+    else{
+        h1sColor = "black";
+    }
+}
+
+h1.addEventListener("click", Run);
