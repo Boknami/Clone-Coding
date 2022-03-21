baaa3f41a17d76274a79ff03eb543fe60c1b4e0c
신근재 <shin75492@naver.com>
2022-03-21 19:14:30 +0900

    ToDoList

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -9,27 +9,42 @@ function App() {
   const onSubmit = (event) => {
     event.preventDefault();
 
-    if (toDo === "")
-    {
+    if(toDo === "") {
       return;
     }
+    //react는 함수를 보낼 때 첫 인자로 현재 상태를 보낸다!
+    setToDos(currentArray => [toDo, ...currentArray]);
     setToDo("");
-  };
-
+  }
+  console.log(toDos);
+  
   return(
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange = {onChange}
-          value={toDo}
-          type="text"
-          placeholder="여기 작성하세요!"
-        />
+  <div>
+    <h1>My To Dos [{toDos.length}]</h1>
+    <form onSubmit={onSubmit}>
+    <input
+      onChange={onChange}
+      value={toDo}
+      type="text"
+      placeholder="Write your to do..."
+    >
+    </input>
 
-        <button>할 일 추가</button>
-      </form>
-    </div>
+    <button>Add To Do</button>
+    </form>
+
+    <hr />
+
+    <ul>
+      {toDos.map((item, index) => (
+      <li key = {index}>{item}</li>
+      ))}
+    </ul>
+  </div>
   );
 }
 
 export default App;
+
+//map은 예전 값들을 불러와서 변형이 가능
+//map의 첫 인자는 예전 값들
